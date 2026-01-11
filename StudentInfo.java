a7a06fb33f85ea086604e0af4cd3f9b1507ddfd2
uneazee <sgh.anisha03@gmail.com>
2026-01-11 22:06:08 +0530

    initial commit: add student info java program

---
+++ StudentInfo.java
@@ -0,0 +1,11 @@
+public class StudentInfo {
+    public static void main(String[] args) {
+        String studentName = "Anisha Singh";
+        String registerNumber = "22MIC0020";
+        String message = "Jenkins-GitHub Integration Successful";
+        
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Register Number: " + registerNumber);
+        System.out.println("Message: " + message);
+    }
+}
